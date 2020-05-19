b86929ddde1ded64c82fd998183b21dea69e120b
Nikkru <56563372+Nikkru@users.noreply.github.com>
2020-05-19 19:25:42 +0300

    Initial Commit

---
+++ HW_2_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_2_1
+//
+//  Created by Nikolai Krusser on 19.05.2020.
+//  Copyright © 2020 Nikolai Krusser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
